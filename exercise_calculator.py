f1a6dd2fea576b76495abb62faa911c8253d648c
joacomoreno-droid <joaquinmorenotdf@gmail.com>
2026-03-30 17:29:07 -0300

    Update exercise_calculator.py

--- exercise_calculator.py
+++ exercise_calculator.py
@@ -23,3 +23,21 @@ def calculator():
         Operacion invalida
     """
     pass
+
+    numero1 = float(input())
+    numero2 = float(input())
+    operacion= input()
+
+    if operacion == "+":
+        print(f"Resultado: {numero1 + numero2}")
+    elif operacion == "-":
+        print(f"Resultado: {numero1 - numero2}")
+    elif operacion == "*":
+        print(f"Resultado: {numero1 * numero2}")
+    elif operacion == "/":
+        if numero2 == 0:
+            print("Error: division por cero")
+        else:
+            print(f"Resultado: {numero1 / numero2}")
+    else:
+        print("Operacion invalida")
