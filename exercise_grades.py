4fcf908b4dbfb369b50fce2f2a8a07af60f339fd
joacomoreno-droid <joaquinmorenotdf@gmail.com>
2026-03-30 17:32:25 -0300

    Update exercise_grades.py

--- exercise_grades.py
+++ exercise_grades.py
@@ -19,3 +19,14 @@ def grades():
         Insuficiente
     """
     pass
+
+    nota = int(input())
+
+    if nota >= 9:
+        print("Excelente")
+    elif nota >= 7 and nota <= 8:
+        print("Bueno")
+    elif nota >= 5 and nota <= 6:
+        print("Regular")
+    elif nota >= 0 and nota <= 4:
+        print("Insuficiente")
